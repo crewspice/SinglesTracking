4d3b6a74934662e313950839622eea1e4671703f
crewspice <160358647+crewspice@users.noreply.github.com>
2024-03-25 15:43:26 -0600

    Update processTracts.R

--- processTracts.R
+++ processTracts.R
@@ -6,3 +6,11 @@ library(sf)
 library(tictoc) # install.packages("tictoc")
 library(mapboxapi) # remotes::install_github('walkerke/mapboxapi')
 options(tigris_use_cache = TRUE)
+
+state_codes <- c(state.abb, "DC")
+names(state_codes) <- state_codes
+age_groups <- read_rds("C:/Users/jacks/Documents/R/ages.rds")
+
+# Cache the blocks to prevent download errors
+# purrr::walk(state_codes, ~blocks(state = .x, year = 2016))
+
