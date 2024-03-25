06601520936b8eeeff741042205be155a801c805
crewspice <160358647+crewspice@users.noreply.github.com>
2024-03-25 13:19:58 -0600

    Update processTracts.R

--- processTracts.R
+++ processTracts.R
@@ -1,3 +1,8 @@
 
 library(tigris)
-library(lehdr) 
+library(lehdr) # remotes::install_github('jamgreen/lehdr')
+library(tidyverse)
+library(sf)
+library(tictoc) # install.packages("tictoc")
+library(mapboxapi) # remotes::install_github('walkerke/mapboxapi')
+options(tigris_use_cache = TRUE)
