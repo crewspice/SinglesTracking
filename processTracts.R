659eba23f97a798afce0f118fba84d02fd165952
crewspice <160358647+crewspice@users.noreply.github.com>
2024-03-25 21:33:13 -0600

    Update processTracts.R

--- processTracts.R
+++ processTracts.R
@@ -14,3 +14,23 @@ age_groups <- read_rds("C:/Users/jacks/Documents/R/ages.rds")
 # Cache the blocks to prevent download errors
 # purrr::walk(state_codes, ~blocks(state = .x, year = 2016))
 
+state_dots <- purrr::map(state_codes, ~{
+  print(sprintf("Processing %s...", .x))
+  state_blocks <- blocks(.x, year = 2016)
+  state_lodes <- grab_lodes(state = .x, year = 2016, lodes_type = "rac",
+                         segment = "S000")
+
+   tic()
+  state_with_rac <- state_blocks %>%
+    filter(GEOID10 %in% state_lodes$h_geocode) %>%
+    transmute(tract_id = str_sub(GEOID10, end = -5)) %>%
+    group_by(tract_id) %>%
+    summarize()
+  toc()
+
+ state_groups <- age_groups %>%
+    filter(str_sub(GEOID, end = 2) == tigris:::validate_state(.x))
+
+    group_names <- names(immigrant_groups)[3:11]
+
+  
