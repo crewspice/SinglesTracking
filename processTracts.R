cd20937a4a28cf99df091c08f515078efcfccfa1
crewspice <160358647+crewspice@users.noreply.github.com>
2024-03-31 20:57:46 -0600

    Update processTracts.R

--- processTracts.R
+++ processTracts.R
@@ -48,9 +48,36 @@ group_names <- names(age_groups)[15:20]
     reduce(rbind) %>%
     slice_sample(prop = 1)
   
-  # Write state dataset to temp file to guard against unforeseen errors
-  write_rds(all_dots, glue::glue("data/temp/{.x}_dots.rds"))
+	# Write state dataset to temp file to guard against unforeseen errors
+	file_path <- file.path("C:/Users/jacks/OneDrive/Documents/R/temp/", paste0(.x, "_dots.rds"))
+	saveRDS(all_dots, file = file_path, compress = FALSE)
+	# write_rds(all_dots, glue::glue("data/temp/{.x}_dots.rds"))
   
   return(all_dots)
 })
   
+
+# Write full dataset to guard against unforeseen errors
+state_dots <- list.files("data/temp", 
+                         pattern = "_dots.rds$", 
+                         full.names = TRUE) %>%
+  map(~read_rds(.x))
+
+write_rds(state_dots, "data/temp/state_dots_list.rds")
+
+# Then, combine
+# state_dots_combined <- read_rds("data/temp/state_dots_list.rds")
+state_dots_combined <- state_dots %>%
+  data.table::rbindlist() %>%
+  st_as_sf(crs = 4326)
+
+tippecanoe(state_dots_combined, 
+           output = "data/state_dots3.mbtiles", 
+           layer_name = "state_dots3",
+           max_zoom = 12,
+           min_zoom = 3,
+           drop_rate = 2)
+
+upload_tiles("data/state_dots3.mbtiles", 
+             username = "crewspice",
+             tileset_name = "state_dots3")
