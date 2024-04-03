fa2de9a3971a1b230edefe28011f6facc8bb9443
crewspice <160358647+crewspice@users.noreply.github.com>
2024-04-02 22:23:55 -0600

    Update processTracts.R

--- processTracts.R
+++ processTracts.R
@@ -9,12 +9,21 @@ options(tigris_use_cache = TRUE)
 
 state_codes <- c(state.abb, "DC")
 names(state_codes) <- state_codes
+
+# Get index of CT and DE in state_codes
+index_CT <- which(state_codes == "CT")
+index_DE <- which(state_codes == "DE")
+
+# Subset state_codes array from CT to DE
+subset_states <- state_codes[index_CT:index_DE]
+
 age_groups <- read_rds("C:/Users/jacks/OneDrive/Documents/R/ages.rds")
 
 # Cache the blocks to prevent download errors
 # purrr::walk(state_codes, ~blocks(state = .x, year = 2016))
 
-state_dots <- purrr::map(state_codes, ~{
+#state_dots <- purrr::map(state_codes, ~{
+state_dots <- purrr::map(subset_states, ~{
   print(sprintf("Processing %s...", .x))
   state_blocks <- blocks(.x, year = 2016)
   state_lodes <- grab_lodes(state = .x, year = 2016, lodes_type = "rac",
@@ -54,16 +63,16 @@ group_names <- names(age_groups)[15:20]
 	# write_rds(all_dots, glue::glue("data/temp/{.x}_dots.rds"))
   
   return(all_dots)
-})
-  
+}) 
+  #
 
 # Write full dataset to guard against unforeseen errors
-state_dots <- list.files("data/temp", 
+state_dots <- list.files("C:/Users/jacks/OneDrive/Documents/R/temp", 
                          pattern = "_dots.rds$", 
                          full.names = TRUE) %>%
   map(~read_rds(.x))
 
-write_rds(state_dots, "data/temp/state_dots_list.rds")
+write_rds(state_dots, "C:/Users/jacks/OneDrive/Documents/R/temp/state_dots_list.rds")
 
 # Then, combine
 # state_dots_combined <- read_rds("data/temp/state_dots_list.rds")
@@ -71,13 +80,25 @@ state_dots_combined <- state_dots %>%
   data.table::rbindlist() %>%
   st_as_sf(crs = 4326)
 
-tippecanoe(state_dots_combined, 
-           output = "data/state_dots3.mbtiles", 
-           layer_name = "state_dots3",
-           max_zoom = 12,
-           min_zoom = 3,
-           drop_rate = 2)
+# Define the command to run Tippecanoe
+command <- "C:\Users\jacks\OneDrive\Documents\Git Repositories\tippecanoe"
+
+# Define Tippecanoe options
+options <- c(
+  "-o data/state_dots3.mbtiles", 
+  "--layer=state_dots3",
+  "--maxzoom=12",
+  "--minzoom=3",
+  "--droprate=2"
+)
+
+# Construct the full command
+full_command <- paste(command, paste(options, collapse = " "), sep = " ")
+
+# Execute the command
+system(full_command)
 
 upload_tiles("data/state_dots3.mbtiles", 
              username = "crewspice",
+	     access_token = "pk.eyJ1IjoiY3Jld3NwaWNlIiwiYSI6ImNsc3M1ZGtwbzBhZ3cyaXJ4YzZvNmRxd20ifQ.UOYUuzL9Aybx7R7Q92A3YQ"
              tileset_name = "state_dots3")
